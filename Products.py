5b22c1c825f515c55c3fd64fb6379e0f7aef1f7d
CallieAnn <dedorec@my.lanecc.edu>
2019-01-14 12:45:11 -0800

    products class and pseudocode for main

---
+++ Products.py
@@ -0,0 +1,38 @@
+class Product:
+    def __init__(self, name, price, quantity, locations):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.locations = locations
+
+        """
+        Greet user
+
+        GetProductInfo()
+
+        create product object 
+        add product object to products collection
+
+        ask if want to add another product
+
+        if yes
+            getProductInfo()
+        end if
+        else
+            output to text file
+            display message to user
+            close program
+        end else
+
+        GetProductInfo()
+        ask for product name
+        validateName()
+        ask for product price
+        validatePrice()
+        ask for product quantity
+        validateQuantity()
+        ask for product locations
+        validateLocations()
+
+        end GetProductInfo
+        """
