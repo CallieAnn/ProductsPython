d38228eb85830a4180d5a1d19f59acca8a145d35
CallieAnn <dedorec@my.lanecc.edu>
2019-01-14 14:15:42 -0800

    user input for products

--- Products.py
+++ Products.py
@@ -1,3 +1,33 @@
+"""
+GetProductInfo()
+ask for product name
+validateName()
+ask for product price
+validatePrice()
+ask for product quantity
+validateQuantity()
+ask for product locations
+validateLocations()
+create product object
+"""
+
+
+def get_user_product():
+    product_name = input("Please enter the product name.")
+    product_price = input("Please enter the product price.")
+    product_quantity = input("Please enter the product quantity.")
+    product_locations = input("Please enter the product locations.")
+
+    user_product = Product(product_name, product_price, product_quantity, product_locations)
+
+    return user_product
+
+
+"""
+end GetProductInfo
+"""
+
+
 class Product:
     def __init__(self, name, price, quantity, locations):
         self.name = name
@@ -5,34 +35,28 @@ class Product:
         self.quantity = quantity
         self.locations = locations
 
-        """
-        Greet user
 
-        GetProductInfo()
+"""Greet user"""
+print("Welcome to the Products program.")
 
-        create product object 
-        add product object to products collection
+"""GetProductInfo()"""
+product = get_user_product()
 
-        ask if want to add another product
+print(product.name, product.price, product.quantity, product.locations)
 
-        if yes
-            getProductInfo()
-        end if
-        else
-            output to text file
-            display message to user
-            close program
-        end else
 
-        GetProductInfo()
-        ask for product name
-        validateName()
-        ask for product price
-        validatePrice()
-        ask for product quantity
-        validateQuantity()
-        ask for product locations
-        validateLocations()
+"""
 
-        end GetProductInfo
-        """
+add product object to products collection
+
+ask if want to add another product
+
+if yes
+    getProductInfo()
+end if
+else
+    output to text file
+    display message to user
+    close program
+end else
+"""
